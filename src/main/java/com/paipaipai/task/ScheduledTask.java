237f7a756f9ce8befa9f0d5ab8b6c16c5ea97585
gluttonweb <384177027@qq.com>
2017-01-07 12:30:24 +0800

    添加开发环境和生产环境配置

---
+++ src/main/java/com/paipaipai/task/ScheduledTask.java
@@ -0,0 +1,21 @@
+package com.paipaipai.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by weibo1 on 2017/1/6.
+ */
+@Component
+public class ScheduledTask {
+
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间是" + simpleDateFormat.format(new Date()));
+    }
+}
