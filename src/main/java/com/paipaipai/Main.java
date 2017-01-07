52624c04d33f2b7fff51bdfef6c2d446856bded7
gluttonweb <384177027@qq.com>
2017-01-07 12:30:43 +0800

    添加开发环境和生产环境配置

--- src/main/java/com/paipaipai/Main.java
+++ src/main/java/com/paipaipai/Main.java
@@ -4,6 +4,7 @@ import com.paipaipai.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @SpringBootApplication
+@EnableScheduling
 public class Main {
 
     @Autowired
