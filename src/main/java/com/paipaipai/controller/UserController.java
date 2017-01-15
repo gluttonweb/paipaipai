8324e65bcf89b9710c24677536560ea1ecaa4021
gluttonweb <384177027@qq.com>
2017-01-15 23:28:29 +0800

    实现redisClient公共服务类

--- src/main/java/com/paipaipai/controller/UserController.java
+++ src/main/java/com/paipaipai/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.paipaipai.controller;
 
 import com.paipaipai.common.RedisClient;
+import com.paipaipai.entity.User;
 import com.paipaipai.mapper.UserMapper;
 import com.paipaipai.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,11 +38,31 @@ public class UserController {
         try {
             if (null == id || id <= 0) return "参数为空";
             String userName = redisClient.get(String.valueOf(id));
+//            if (StringUtils.isEmpty(userName))
+
             return userName;
         } catch (Exception e) {
             e.printStackTrace();
             return e;
         }
+    }
+
+    @RequestMapping("/getUserById/{id}")
+    public Object getUserById(@PathVariable("id") Integer id) {
+        User user = new User();
+        try {
+            if (null == id || id <= 0) return "参数为空";
+            user = redisClient.get(String.valueOf(id), User.class);
+            if (null == user) {
+                user = this.userMapper.selectByPrimaryKey(id);
+                if (null != user) {
+                    redisClient.set(String.valueOf(id), user);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
 }
